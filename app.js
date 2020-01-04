8e5cb23c50424ce74256e0c623e5efe6265aa109
Marcin Dzieszkowski <m.dzieszkowski@gmail.com>
2020-01-04 21:13:31 +0100

    add phone number option

--- app.js
+++ app.js
@@ -239,7 +239,9 @@ for (let i = 0; i < 20; i++) {
     Math.random() * (999999999 - 100000000 + 1) + 100000000
   );
 
-  const personalData = { gender, firstName, lastName, age, phone };
+  const email = firstName + '.' + lastName + '@gmail.com';
+
+  const personalData = { gender, firstName, lastName, age, phone, email };
   people.push(personalData);
 }
 
